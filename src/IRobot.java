100d87f854969c890dae8ce3d5ecd2d6b00210c2
Anusha Tarla <atarla@uncc.edu>
2016-11-15 09:03:35 -0500

    Added Classes

---
+++ src/IRobot.java
@@ -0,0 +1,10 @@
+/**
+ * Created by anush on 11/15/2016.
+ */
+public interface IRobot {
+    public void turnOn();
+    public void turnOff();
+    public void setSpeed();
+    public void move();
+    public void turn();
+}
---
+++ src/LegoNXTVehicle.java
@@ -0,0 +1,21 @@
+/**
+ * Created by anush on 11/15/2016.
+ */
+public class LegoNXTVehicle {
+    public void LegoNXTVehicle()
+    {
+
+    }
+    public void turnOn()
+    {
+
+    }
+    public void move()
+    {
+
+    }
+    public void setSpeed()
+    {
+
+    }
+}
